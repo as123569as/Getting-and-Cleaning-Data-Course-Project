7863b87e30cb1124517cc496d32ec94d388b54eb
as123569as <35484736+as123569as@users.noreply.github.com>
2018-01-19 17:22:49 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -17,34 +17,34 @@ if (!file.exists("UCI HAR Dataset")) {
    unzip("dataset.zip") 
 }
 
-# Import activity labels and features
+# Import activity labels and features:
 activitylabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 features <- read.table("UCI HAR Dataset/features.txt")
 
-# Extract only the data on mean and standard deviation
+# Extract only the data on mean and standard deviation:
 featuresWanted <- grep(".*mean.*|.*std.*", features[,2])    #pick the items that contain "mean" and "std"
 featuresWanted.names <- features[featuresWanted,2]          #use the items just picked to select the features that contain "mean" and "std"
 
-# Import the train datasets
+# Import the train datasets:
 train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
 trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
 trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
 train <- cbind(trainSubjects, trainActivities, train)
 
-# Import the test datasets
+# Import the test datasets:
 test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
 testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
 testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 test <- cbind(testSubjects, testActivities, test)
 
-# merge datasets and add labels
+# merge datasets and add labels:
 TotalData <- rbind(train, test)
 colnames(TotalData) <- c("subject", "activity", as.character(featuresWanted.names))
 TotalData$activity <- factor(TotalData$activity, levels = activitylabels[,1], labels = activitylabels[,2])
 
-# Calculate the mean of each variable
+# Calculate the mean of each variable:
 TotalData.melt <- melt(TotalData, id = c("subject", "activity"))
 TotalData.mean <- dcast(TotalData.melt, subject + activity ~ variable, mean)
 
-# Export the result into txt file
+# Export the result into txt file:
 write.table(TotalData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
