a185929d8e5795d565efc3209c7f203845165354
as123569as <35484736+as123569as@users.noreply.github.com>
2018-01-19 17:22:20 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,4 @@
-#library(dplyr)
 library(reshape2)
-
-activity_labels <- read.table("C:/Users/ur06378/Desktop/資料彙整/課程/Coursera-資料清洗課程/Project/UCI HAR Dataset/activity_labels.txt")
-activity_labels <- rename(activity_labels, item = "V1", activity = "V2")
-
 
 ## Create and set working directory:
 if (!file.exists("Project Data")){
@@ -24,17 +19,11 @@ if (!file.exists("UCI HAR Dataset")) {
 
 # Import activity labels and features
 activitylabels <- read.table("UCI HAR Dataset/activity_labels.txt")
-#activitylabels[,2] <- as.character(activitylabels[,2])
 features <- read.table("UCI HAR Dataset/features.txt")
-#features[,2] <- as.character(features[,2])
 
 # Extract only the data on mean and standard deviation
 featuresWanted <- grep(".*mean.*|.*std.*", features[,2])    #pick the items that contain "mean" and "std"
 featuresWanted.names <- features[featuresWanted,2]          #use the items just picked to select the features that contain "mean" and "std"
-#featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
-#featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
-#featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
-
 
 # Import the train datasets
 train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
@@ -50,12 +39,12 @@ test <- cbind(testSubjects, testActivities, test)
 
 # merge datasets and add labels
 TotalData <- rbind(train, test)
-colnames(TotalData) <- c("subject", "activity", featuresWanted.names)
+colnames(TotalData) <- c("subject", "activity", as.character(featuresWanted.names))
 TotalData$activity <- factor(TotalData$activity, levels = activitylabels[,1], labels = activitylabels[,2])
 
 # Calculate the mean of each variable
 TotalData.melt <- melt(TotalData, id = c("subject", "activity"))
 TotalData.mean <- dcast(TotalData.melt, subject + activity ~ variable, mean)
 
-# Export the result into .txt file
+# Export the result into txt file
 write.table(TotalData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
